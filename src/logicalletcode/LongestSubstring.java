36c23b427df155aab3e6419ce25c0af7d51e9dbc
Palash <thinkert26@gmail.com>
2023-11-03 18:44:51 +0530

    LongestSubstring

--- src/logicalletcode/LongestSubstring.java
+++ src/logicalletcode/LongestSubstring.java
@@ -1,5 +1,24 @@
 package logicalletcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstring {
+
+        public int longsubstring(String s) {
+            int n = s.length();
+            Set<Character> set = new HashSet<>();
+
+            return 0;
+        }
+
+
+    public void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+        Solution solution = new Solution();
+        String input = "abcabcabc";
+        int result = longestSubstring.longsubstring(input);
+        System.out.println(result);
+    }
 
 }
